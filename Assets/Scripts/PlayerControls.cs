48428a0972c4b2dff02fb2361ae1100b4d1176e7
Rafal Mi <rafal.m4@gmail.com>
2023-09-17 16:32:55 +0200

    04 77 Move Player Using Input

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -7,6 +7,7 @@ using UnityEngine.InputSystem;
 public class PlayerControls : MonoBehaviour
 {
     // [SerializeField] InputAction movement;
+    [SerializeField] float controlSpeed = 10f;
 
     // Start is called before the first frame update
     void Start()
@@ -34,10 +35,13 @@ public class PlayerControls : MonoBehaviour
         float xThrow = Input.GetAxis("Horizontal");
         float yThrow = Input.GetAxis("Vertical");
 
-        float xOffset = .1f;
+        float xOffset = xThrow * Time.deltaTime * controlSpeed;
         float newXPos = transform.localPosition.x + xOffset;
 
-        transform.localPosition = new Vector3(newXPos, transform.localPosition.y, transform.localPosition.z);
+        float yOffset = yThrow * Time.deltaTime * controlSpeed;
+        float newYPos = transform.localPosition.y + yOffset;
+
+        transform.localPosition = new Vector3(newXPos, newYPos, transform.localPosition.z);
 
 
     }
